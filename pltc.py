f6f1f8c7db64ad366b9716bff80334ba7a85f5d3
deadManAlive <66319773+deadManAlive@users.noreply.github.com>
2020-08-05 22:04:16 +0700

    create cwd version

---
+++ pltc.py
@@ -0,0 +1,37 @@
+from imgFunc import renamer, imageCnR, stopper
+import os, shutil
+
+
+path = os.path.join(os.environ['USERPROFILE'], 'Pictures')
+
+cache, target = os.path.join(os.getcwd(), r'cache'), os.path.join(os.getcwd(), r'target')
+
+if not os.path.exists(cache):
+    os.makedirs(cache)
+if not os.path.exists(target):
+    os.makedirs(target)
+
+for _, _, imgs in os.walk(target):
+    if imgs:
+        for img in imgs:
+            os.remove(os.path.join(target, img))
+        
+
+files = sorted([os.path.join(root,f) for root,_,the_files in os.walk(path) for f in the_files if (f.lower().endswith(".jpg") or f.lower().endswith(".jpeg"))], key=os.path.getctime, reverse = True)
+
+del files[5:]
+
+for _, _, imgs in os.walk(cache):
+    if imgs:
+        for oldImg in imgs:
+            os.remove(os.path.join(cache, oldImg))
+
+for image in files:
+    shutil.copy(image, cache)
+
+renamer(cache)
+
+cacheList = os.listdir(cache)
+
+for image in cacheList:
+    imageCnR(image, cache, target)
--- suBatch.bat
+++ suBatch.bat
@@ -1,3 +1,3 @@
 cd %~dp0
 dir
-"C:/Program Files (x86)/Python38-32/python.exe" picUpdtr.py
+"C:/Program Files (x86)/Python38-32/python.exe" pltc.py
